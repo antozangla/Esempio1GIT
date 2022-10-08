b1208ed71f7114d6cbd784498c96ea50ed7bfd75
Antonio Zangla <ANTONIO.ZANGLA@local.ittcesena.it>
2022-10-08 08:38:21 +0200

    Versione base: calcolo media

---
+++ GIT_es1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GIT_es1
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEsegui_Click(object sender, RoutedEventArgs e)
+        {
+            double[] v= new double[10];
+
+            Random rnd= new Random();
+            for(int i=0; i<v.Length; i++)
+            {
+                v[i] = rnd.Next(0,100);
+            }
+            double somma = 0;
+            for(int i=0; i<v.Length; i++)
+            {
+                somma += v[i];
+            }
+            lblMain.Content = somma / v.Length;
+        }
+    }
+}
